667a447c6c854c85f048b1825bdbf7f4d08bc850
Ricardo Casares <ricardocasares@gmail.com>
2019-05-21 01:29:53 +0200

    remove unused code

--- src/router/reopen/index.ts
+++ src/router/reopen/index.ts
@@ -4,20 +4,5 @@ import { getAccount } from "./getAccount";
 import { unlock } from "./unlock";
 import { respond } from "./respond";
 import { email } from "./email";
-import { Middleware } from "@/models";
 
-const mw: Middleware = async (ctx, next) => {
-  ctx.log.info(ctx.data, "DATA");
-  ctx.data = { fun: "tastic" };
-
-  await next();
-};
-
-export const reopen = compose([
-  mw,
-  validate,
-  getAccount,
-  unlock,
-  email,
-  respond
-]);
+export const reopen = compose([validate, getAccount, unlock, email, respond]);
